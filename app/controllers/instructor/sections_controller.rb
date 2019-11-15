2925a06980870458b8a3b5a8961ece7ba2351a36
Jennifer <jenniferkjerstad@gmail.com>
2019-11-15 21:10:08 +0000

    lessonspage

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -7,18 +7,18 @@ class Instructor::SectionsController < ApplicationController
   end
 
   def create
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   private
 
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render plain: "Unauthorized", status:unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
-  
+
   helper_method :current_course
   def current_course
     @current_course ||= Course.find(params[:course_id])
